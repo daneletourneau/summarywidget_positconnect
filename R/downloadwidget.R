002b6111cfff8bd5d11ac2816074999ef42b13f7
Dane Letourneau <letourneau.dane@gmail.com>
2023-04-25 13:08:40 -0700

    Update downloadwidget.R

--- R/downloadwidget.R
+++ R/downloadwidget.R
@@ -96,7 +96,7 @@ downloadwidget <- function(data,
 #' @param width,height Must be a valid CSS unit (like \code{'100\%'},
 #'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
 #'   string and have \code{'px'} appended.
-#' @param expr An expression that generates a summarywidget
+#' @param expr An expression that generates a downloadwidget
 #' @param env The environment in which to evaluate \code{expr}.
 #' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
 #'   is useful if you want to save an expression in a variable.
@@ -105,10 +105,10 @@ downloadwidget <- function(data,
 #'
 #' @export
 downloadwidgetOutput <- function(outputId, width = '100%', height = '400px'){
-  htmlwidgets::shinyWidgetOutput(outputId, 'downloadwidget', width, height, package = 'summarywidget')
+  htmlwidgets::shinyWidgetOutput(outputId, 'downloadwidget', width, height, package = 'downloadwidget')
 }
 
-#' @rdname summarywidget-shiny
+#' @rdname downloadwidget-shiny
 #' @export
 renderDownloadwidget <- function(expr, env = parent.frame(), quoted = FALSE) {
   if (!quoted) { expr <- substitute(expr) } # force quoted
